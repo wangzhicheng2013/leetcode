1bdab262e6783fed0a1628dad74b3537ae1ebf78
wb7898 <wb7898@arcsoft.com.cn>
2023-04-12 14:28:57 +0800

    1

--- leetcode/前 K 个高频元素/test.cpp
+++ leetcode/前 K 个高频元素/test.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
+#include <queue>
 #include <algorithm>
 using namespace std;
+struct Less {
+    bool operator() (const std::pair<int, int>& e1, const std::pair<int, int>& e2) const {
+        return e1.second < e2.second;
+    }
+};
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
@@ -17,7 +23,7 @@ public:
             }
         }
         vec.clear();
-        for (auto m : stat_map) {
+        for (const auto &m : stat_map) {
             vec.push_back(m);    
         }
         sort(vec.begin(), vec.end(), [] (const std::pair<int, int>& e1, const std::pair<int, int>& e2) { return e1.second > e2.second; } );
@@ -27,8 +33,33 @@ public:
         }
         return tmp;
     }
+    vector<int> topKFrequent1(vector<int>& nums, int k) {
+        stat_map.clear();
+        for (auto &num : nums) {
+            auto it = stat_map.find(num);
+            if (it == stat_map.end()) {
+                stat_map.emplace(num, 1);
+            }
+            else {
+                ++(it->second);
+            }
+        }
+        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, Less>pq;
+        for (const auto &m : stat_map) {
+            pq.push(m);    
+        }
+        if (k > pq.size()) {
+            k = pq.size();
+        }
+        std::vector<int>tmp;
+        for (int i = 0;i < k;i++) {
+            tmp.push_back(pq.top().first);
+            pq.pop();
+        }
+        return tmp;
+    }
 private:
-    std::map<int, int>stat_map;
+    std::unordered_map<int, int>stat_map;
     std::vector<std::pair<int, int> >vec;
 };
 int main() {
